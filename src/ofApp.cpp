dd29f270186b2360a196ea534c8080b0f2531b1f
Ashley Kim <kitetale248@gmail.com>
2023-04-19 11:16:24 -0400

    Polyline diff on Kinect

---
+++ src/ofApp.cpp
@@ -0,0 +1,147 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofBackground(255); //white
+    
+    //kinect.setLogLevel(OF_LOG_NOTICE);
+    kinect.init();
+    kinect.open();
+    imitate(imgPx,kinect);
+    
+    h = kinect.height;
+    w = kinect.width;
+    
+    nearClip = 4000; // in mm
+    farClip = 10000; // in mm
+    kinect.setDepthClipping(nearClip,farClip);
+    
+    colorImage.allocate(w,h);
+    grayImage.allocate(w,h);
+    grayBg.allocate(w,h);
+    grayDiff.allocate(w,h);
+    
+    grayThreshold = 30;
+    learnBg = false;
+    
+    output.allocate(w,h,OF_IMAGE_COLOR);
+    
+    font.load("Montserrat.ttf", 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    kinect.update();
+    if (kinect.isFrameNew()){
+        colorImage.setFromPixels(kinect.getPixels());
+        grayImage.setFromPixels(kinect.getDepthPixels());
+        
+        absdiff(kinect,prevPx,imgDiff);
+        imgDiff.update();
+        imgDiff.mirror(false, true);
+        copy(kinect,prevPx);
+        
+        grayDiff.absDiff(grayBg, grayImage);
+        grayDiff.threshold(grayThreshold);
+        contourFinder.findContours(grayImage, 30, (w*h), 10, true);
+                
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+    kinect.setCameraTiltAngle(0);
+    kinect.close();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    //kinect.draw(0,0,w,h);
+    
+    ofSetColor(0);
+    for (int i = 0; i < kinect.getHeight(); i+=8){
+        ofPolyline polyline;
+        for (int j = 0; j < kinect.getWidth(); j++){
+            ofColor col = imgDiff.getPixels().getColor(j,i);
+            int brightness = col.getBrightness();
+            polyline.addVertex(j, i+ofMap(brightness, 0, 255, 0, -64));
+            //sends y coord up if bright
+        }
+        polyline = polyline.getSmoothed(10);
+        polyline.draw();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key){
+        case OF_KEY_UP:
+            if (angle < 30) {
+                ++angle;
+            }
+            kinect.setCameraTiltAngle(angle);
+            break;
+
+        case OF_KEY_DOWN:
+            if (angle > -30) {
+                --angle;
+            }
+            kinect.setCameraTiltAngle(angle);
+            break;
+        
+        default:
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxKinect.h"
+#include "ofxOpenCv.h"
+#include "ofxCv.h"
+
+using namespace ofxCv;
+using namespace cv;
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+        void exit();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    
+    ofxKinect kinect;
+    ofxCvColorImage colorImage;
+    ofImage imgDiff;
+    ofPixels prevPx;
+    ofPixels imgPx;
+    ofxCvGrayscaleImage grayImage, grayBg, grayDiff;
+    ofxCvContourFinder contourFinder;
+    int grayThreshold;
+    bool learnBg;
+    ofImage output; //ofImage for saving to os
+    
+    ofTrueTypeFont font;
+    
+    int h,w;
+    int nearClip,farClip;
+    int angle;
+
+};
