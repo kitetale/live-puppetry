4d6b08bbb51fc989bcdfc5ae2dc945229bac8f8a
Ashley Kim <kitetale248@gmail.com>
2023-05-08 01:46:06 -0400

    superb bird using spheres

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -14,18 +14,19 @@ void ofApp::setup(){
     gui.add(camYPos.setup("cam y pos",395, -500, 500));
     gui.add(camZPos.setup("cam z pos", 0, -500, 500));
     gui.add(showHand);
-    /*model.loadModel("hand.obj", 20);
-    curFileInfo = ".obj";
-    
-    model.setRotation(0, 0, 0, 0, 0);
-    model.setScale(1,1,1);
-     */
     light.setPosition(0, 0, 500);
     
     cameraOrbit = 0;
     cam.setDistance(750);
     
     osc.setup(PORT);
+    
+    head.setRadius(100);
+    legL.setRadius(20);
+    legR.setRadius(20);
+    eyeL.setRadius(7);
+    eyeR.setRadius(7);
+    nose.setRadius(60);
     
     sphere.setRadius(20);
     handSpheres.push_back(sphere); //wrist
@@ -62,75 +63,54 @@ void ofApp::update(){
                 handXs[i] = m.getArgAsFloat(4+i*3);
                 handYs[i] = m.getArgAsFloat(5+i*3);
             }
-            
-            
-            /*
-            //thumb xy
-            x1 = m.getArgAsFloat(7); y1 = m.getArgAsFloat(8);
-            x2 = m.getArgAsFloat(10); y2 = m.getArgAsFloat(11);
-            x3 = m.getArgAsFloat(13); y3 = m.getArgAsFloat(14);
-            x4 = m.getArgAsFloat(16); y4 = m.getArgAsFloat(17);
-            //index xy
-            x5 = m.getArgAsFloat(19); y5 = m.getArgAsFloat(20);
-            x6 = m.getArgAsFloat(22); y6 = m.getArgAsFloat(23);
-            x7 = m.getArgAsFloat(25); y7 = m.getArgAsFloat(26);
-            x8 = m.getArgAsFloat(28); y8 = m.getArgAsFloat(29);
-            //middle xy
-            x9  = m.getArgAsFloat(31); y9  = m.getArgAsFloat(32);
-            x10 = m.getArgAsFloat(34); y10 = m.getArgAsFloat(35);
-            x11 = m.getArgAsFloat(37); y11 = m.getArgAsFloat(38);
-            x12 = m.getArgAsFloat(40); y12 = m.getArgAsFloat(41);
-            //ring xy
-            x13 = m.getArgAsFloat(43); y13 = m.getArgAsFloat(44);
-            x14 = m.getArgAsFloat(46); y14 = m.getArgAsFloat(47);
-            x15 = m.getArgAsFloat(49); y15 = m.getArgAsFloat(50);
-            x16 = m.getArgAsFloat(52); y16 = m.getArgAsFloat(53);
-            //pinky xy
-            x17 = m.getArgAsFloat(55); y17 = m.getArgAsFloat(56);
-            x18 = m.getArgAsFloat(58); y18 = m.getArgAsFloat(59);
-            x19 = m.getArgAsFloat(61); y19 = m.getArgAsFloat(62);
-            x20 = m.getArgAsFloat(64); y20 = m.getArgAsFloat(65);
-            */
-            
         }
-        
     }
-    /*
-    vector<glm::vec3> vertices0 = sphere.getMesh().getVertices();
-    vector<glm::vec3> &vertices = sphere.getMesh().getVertices();
-    for (int i=0; i<vertices.size(); i++) {
-      ofPoint v = vertices0[i];
-      v.normalize();
-      float sx = sin( v.x * deformFreq );
-      float sy = sin( v.y * deformFreq );
-      float sz = sin( v.z * deformFreq );
-      v.x += sy * sz * deform;
-      v.y += sx * sz * deform;
-      v.z += sx * sy * deform;
-      v *= rad;
-      vertices[i] = v;
-    }*/
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     ofEnableDepthTest();
-    
     light.enable();
-
     cam.begin();
-/*
-    ofColor(255,255);
-    ofPushMatrix();
-    ofTranslate(-oscx*ofGetWidth()/w,-oscy*ofGetHeight()/h);
-    model.drawFaces();
-    ofPopMatrix();
-   
-  */
     
-    //osc tester
-    //ofDrawCircle(oscx*ofGetWidth()/w, oscy*ofGetHeight()/h, 50);
+    head.setPosition(0,0,0);
+    legL.setPosition(35,-20,0);
+    legR.setPosition(-35,-20,0);
+    eyeL.setPosition(20, 0, 80);
+    eyeR.setPosition(-20, 0, 80);
+    nose.setPosition(0,-70,40);
+    
+    ofPushMatrix();
+        ofTranslate(-100,-100);
+        ofPushStyle();
+        ofSetColor(30, 30, 50);
+            ofPushMatrix();
+                ofScale(1.3,1,0.7);
+                head.draw();
+            ofPopMatrix();
+        
+            ofPushMatrix();
+                ofScale(0.4,4,1);
+                legL.draw();
+                legR.draw();
+            ofPopMatrix();
+        ofPopStyle();
+        ofPushStyle();
+        ofSetColor(0,128,128);
+            eyeL.draw();
+            eyeR.draw();
+            ofPushMatrix();
+                ofRotateDeg(-30, 10, 0, 0);
+                ofScale(1.7,0.8,0.3);
+                nose.draw();
+            ofPopMatrix();
+        ofPopStyle();
+    
+    ofPopMatrix();
+    
+    
+    
+    //debug
     if (showHand){
         ofPushMatrix();
         ofPushStyle();
@@ -149,11 +129,8 @@ void ofApp::draw(){
     }
     
     cam.end();
-    
     light.disable();
-
     ofDisableDepthTest();
-    
     
     gui.draw();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -27,9 +27,12 @@ class ofApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
 		
     ofxAssimpModelLoader model;
+    
     ofSpherePrimitive sphere;
-    //ofSpherePrimitive s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20;
     vector<ofSpherePrimitive> handSpheres;
+    ofSpherePrimitive head, legL, legR, eyeL, eyeR, nose;
+    
+    
     ofLight light;
     
     string curFileInfo;
@@ -41,18 +44,13 @@ class ofApp : public ofBaseApp{
     
     //hand : https://developers.google.com/mediapipe/solutions/vision/hand_landmarker
     float oscx = 0.0; float oscy = 0.0; //wrist
-    float x1,y1,x2,y2,x3,y3,x4,y4; //thumb
-    float x5,y5,x6,y6,x7,y7,x8,y8; //index
-    float x9,y9,x10,y10,x11,y11,x12,y12; //middle
-    float x13,y13,x14,y14,x15,y15,x16,y16; //ring
-    float x17,y17,x18,y18,x19,y19,x20,y20; //pinky
     vector<float> handXs, handYs;
     
     float w, h;
     
     ofxFloatSlider rad, deform, deformFreq, extrude;
     ofxFloatSlider camXPos, camYPos, camZPos;
-    ofParameter<bool> showHand = {"Show Hand", true};
+    ofParameter<bool> showHand = {"Show Hand", false};
     ofxPanel gui;
         
 };
